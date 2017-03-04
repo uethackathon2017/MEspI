8fc859b78a0c2c7be4de8848b5d34b3dfde1daf7
TranAn <trantuanan0812196@gmail.com>
2017-03-05 00:41:18 +0700

    Initial Commit

---
+++ MapGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapGame
+//
+//  Created by Trần An on 3/5/17.
+//  Copyright © 2017 TranAn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
