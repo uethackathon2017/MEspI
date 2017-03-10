836729ceadfff6ac88fdb1e0609c7599c999107d
Os <os@Oss-MacBook-Pro.local>
2017-03-10 19:57:48 +0700

    Initial commit

---
+++ MapGame/PlaylistViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlaylistViewController.swift
+//  MapGame
+//
+//  Created by Os on 3/10/17.
+//  Copyright © 2017 TranAn. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
